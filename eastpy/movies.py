56b776054246e30c5ff8ac6308618ea15348fef3
Andrew M. Elgert <andrew.elgert@gmail.com>
2015-10-03 11:33:15 -0400

    Added formatting
    - Implemented JSONMovieFormatter which outputs each movie as single JSON object
    - Implemented JSONArrayMovieFormatter which outputs a JSON array of movie JSON objects
    - Implemented SimpleStringMovieFormatter which uses the original formatting

--- eastpy/movies.py
+++ eastpy/movies.py
@@ -2,11 +2,28 @@ from __future__ import absolute_import, print_function, division
 
 import abc
 import sys
+import json
+
+import attr
 
 from typing import Callable, IO, cast
 
 __author__ = 'Andrew Elgert, James Ladd'
 __credits__ = ['Andrew Elgert', 'James Ladd']
+
+
+@attr.s
+class MovieValue(object):
+    title = attr.ib(default='')
+    director = attr.ib(default='')
+
+
+class MovieFormatter(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def format_and_print_on(self, movie: MovieValue, stream: IO[str]) -> 'MovieFormatter':
+        """Format :movie and print onto :streamwriter"""
 
 
 class Movie(object):
@@ -21,7 +38,7 @@ class Movie(object):
         """Perform :action if movie title is :title"""
 
     @abc.abstractmethod
-    def print_on_with_format(self, stream: IO[str]) -> 'Movie':
+    def print_on_with_format(self, stream: IO[str], formatter: MovieFormatter) -> 'Movie':
         """Print movie onto :stream using :format"""
 
 
@@ -49,6 +66,36 @@ class MoviesClient(object):
         """Append :movie onto stream using :formatter"""
 
 
+class SimpleStringMovieFormatter(MovieFormatter):
+    def format_and_print_on(self, movie: MovieValue, stream: IO[str]):
+        stream.write('Movie (title: {}, director: {})\n'.format(movie.title, movie.director))
+        return self
+
+
+class JSONMovieFormatter(MovieFormatter):
+    def format_and_print_on(self, movie: MovieValue, stream: IO[str]):
+        stream.write(json.dumps(attr.asdict(movie)))
+        stream.write('\n')
+        return self
+
+
+class JSONArrayMovieFormatter(MovieFormatter):
+    def __init__(self):
+        self._array = []
+        self._stream = None
+
+    def format_and_print_on(self, movie: MovieValue, stream: IO[str]):
+        if self._stream is None:
+            self._stream = stream
+        self._array.append(attr.asdict(movie))
+        stream.write(json.dumps(self._array))
+        stream.write('\n')
+
+    def collect_and_print(self):
+        if len(self._array) > 0 and self._stream is not None:
+            self._stream.write(json.dumps(self._array))
+
+
 class ExampleMovieLister(MovieLister):
     def __init__(self, finder: MovieFinder):
         self._finder = finder
@@ -73,8 +120,9 @@ class ExampleMovie(Movie):
             action(self)
         return self
 
-    def print_on_with_format(self, stream: IO[str]):
-        stream.write('Movie (title: {}, director: {})\n'.format(self._title, self._director))
+    def print_on_with_format(self, stream: IO[str], formatter: MovieFormatter):
+        movie = MovieValue(title=self._title, director=self._director)
+        formatter.format_and_print_on(movie, stream)
         return self
 
 
@@ -94,34 +142,39 @@ class ExampleMovieFinder(MovieFinder):
 
 
 class ExampleMoviesClientStreamAdaptor(MoviesClient):
-    def __init__(self, stream: IO[str]):
+    def __init__(self, stream: IO[str], formatter: MovieFormatter):
         self._stream = stream
+        self._formatter = formatter
 
     def append(self, movie: Movie):
-        movie.print_on_with_format(self._stream)
+        movie.print_on_with_format(self._stream, self._formatter)
 
 
 class ExampleMoviesClientFileAdaptor(MoviesClient):
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, formatter: MovieFormatter):
         self._file_path = file_path
+        self._formatter = formatter
 
     def append(self, movie: Movie):
         with cast(IO[str], open(self._file_path, 'a')) as f:
-            movie.print_on_with_format(f)
+            f.truncate()
+            movie.print_on_with_format(f, self._formatter)
 
 
 if __name__ == '__main__':
     ExampleMovieLister(ExampleMovieFinder()).apply_to_movies_directed_by(
-        lambda movie: ExampleMoviesClientStreamAdaptor(sys.stdout).append(movie),
+        lambda movie: ExampleMoviesClientStreamAdaptor(sys.stdout, JSONMovieFormatter()).append(movie),
         'George Lucas'
     )
 
     ExampleMovieLister(ExampleMovieFinder()).apply_to_movies_directed_by(
-        lambda movie: ExampleMoviesClientStreamAdaptor(sys.stdout).append(movie),
+        (lambda formatter: lambda movie: ExampleMoviesClientStreamAdaptor(sys.stdout, formatter).append(movie))(
+            JSONArrayMovieFormatter()
+        ),
         'David Lynch'
     )
 
     ExampleMovieLister(ExampleMovieFinder()).apply_to_movies_directed_by(
-        lambda movie: ExampleMoviesClientFileAdaptor("../movies.txt").append(movie),
-        'David Cronenberg'
+        lambda movie: ExampleMoviesClientFileAdaptor("../movies.txt", SimpleStringMovieFormatter()).append(movie),
+        'David Lynch'
     )
